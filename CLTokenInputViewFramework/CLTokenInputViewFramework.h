260985153328d991ee8e66a3cbcaec7ad55e6cf5
Gianni Carlo <gcarlo89@hotmail.com>
2018-04-05 16:04:21 -0500

    Add framework target

---
+++ CLTokenInputViewFramework/CLTokenInputViewFramework.h
@@ -0,0 +1,19 @@
+//
+//  CLTokenInputViewFramework.h
+//  CLTokenInputViewFramework
+//
+//  Created by Gianni Carlo on 4/5/18.
+//  Copyright © 2018 Cluster Labs, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CLTokenInputView/CLTokenInputView.h>
+//! Project version number for CLTokenInputViewFramework.
+FOUNDATION_EXPORT double CLTokenInputViewFrameworkVersionNumber;
+
+//! Project version string for CLTokenInputViewFramework.
+FOUNDATION_EXPORT const unsigned char CLTokenInputViewFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CLTokenInputViewFramework/PublicHeader.h>
+
+
---
+++ CLTokenInputViewFrameworkTests/CLTokenInputViewFrameworkTests.swift
@@ -0,0 +1,36 @@
+//
+//  CLTokenInputViewFrameworkTests.swift
+//  CLTokenInputViewFrameworkTests
+//
+//  Created by Gianni Carlo on 4/5/18.
+//  Copyright © 2018 Cluster Labs, Inc. All rights reserved.
+//
+
+import XCTest
+@testable import CLTokenInputViewFramework
+
+class CLTokenInputViewFrameworkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
